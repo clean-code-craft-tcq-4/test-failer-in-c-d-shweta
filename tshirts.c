fe7a974d53d75f59ff1ebc9a1b6c0c275c6830d9
d-shweta <105386129+d-shweta@users.noreply.github.com>
2022-06-22 02:46:18 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <assert.h>
 
+void testSsize(void);
+void testMsize(void);
+void testLsize(void);
+
 char size(int cms) {
     char sizeName = '\0';
     if(cms < 38) {
@@ -13,10 +17,26 @@ char size(int cms) {
     return sizeName;
 }
 
-int main() {
+void testSsize(){
     assert(size(37) == 'S');
+    assert(size(38) == 'S');
+}
+
+void testMsize(){
+    assert(size(39) == 'M');
     assert(size(40) == 'M');
-    assert(size(43) == 'L');
+    assert(size(42) == 'M');
+}
+
+void testLsize(){
+     assert(size(43) == 'L');
+}
+
+
+int main() {
+    testSsize();
+    testMsize();
+    testLsize();
     printf("All is well (maybe!)\n");
     return 0;
 }
